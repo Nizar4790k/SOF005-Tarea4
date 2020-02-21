16ef62886796040f39a47ecd2c5b3b61466ffaf2
Nizar <nizar4790k@gmail.com>
2020-02-21 09:36:00 -0400

    se definio la funcion de convertir longitud

--- Controllers/UnityController.cs
+++ Controllers/UnityController.cs
@@ -20,10 +20,10 @@ namespace SOF005_Tarea4.Controllers
 
 
 
-        private double ConvertirMasa(double cantidadAconvertir, Masa unidadOrigen, Masa unidadDestino)
+        private double ConvertirLongitud(double cantiadadAconveritr,Longitud unidadOrigen, Longitud unidadDestino)
         {
 
-            // En esta funcion se escogio el KILOGRAMO como unidad base
+            // En esta funcion se escogio el METRO como unidad base
 
             if (unidadOrigen == unidadDestino)
             {
@@ -34,7 +34,7 @@ namespace SOF005_Tarea4.Controllers
             {
 
                 case Masa.GRAMO:
-                    cantidadAconvertir = cantidadAconvertir/1000;
+                    cantidadAconvertir = cantidadAconvertir / 1000;
                     break;
 
                 case Masa.ONZA:
@@ -71,16 +71,93 @@ namespace SOF005_Tarea4.Controllers
             {
 
                 case Masa.GRAMO:
-                     return cantidadAconvertir = cantidadAconvertir * 1000;
-                 
+                    return cantidadAconvertir = cantidadAconvertir * 1000;
+
 
                 case Masa.ONZA:
                     return cantidadAconvertir = cantidadAconvertir * 35.274;
                     break;
 
                 case Masa.LIBRA:
-                   return  cantidadAconvertir = cantidadAconvertir * 2.205;
-                 
+                    return cantidadAconvertir = cantidadAconvertir * 2.205;
+
+
+            }
+
+
+            return cantidadAconvertir;
+
+
+
+
+        }
+
+
+
+
+
+        private double ConvertirMasa(double cantidadAconvertir, Masa unidadOrigen, Masa unidadDestino)
+        {
+
+            // En esta funcion se escogio el KILOGRAMO como unidad base
+
+            if (unidadOrigen == unidadDestino)
+            {
+                return cantidadAconvertir;
+            }
+
+            switch (unidadOrigen)
+            {
+
+                case Longitud.CENTIMETRO:
+                    cantidadAconvertir = cantidadAconvertir/100;
+                    break;
+
+                case Longitud.KILOMETRO:
+                    cantidadAconvertir = cantidadAconvertir * 1000;
+                    break;
+
+                case Longitud.MILLA:
+                    cantidadAconvertir = cantidadAconvertir * 1609;
+                    break;
+
+                case Longitud.PULGADAS:
+                    cantidadAconvertir = cantidadAconvertir / 39.37;
+                    break;
+
+                case Longitud.PIE:
+                    cantidadAconvertir = cantidadAconvertir / 3.281;
+                    break;
+
+
+            }
+
+
+
+
+            switch (unidadDestino)
+            {
+
+                case Longitud.CENTIMETRO:
+                    cantidadAconvertir = cantidadAconvertir * 100;
+                    break;
+
+                case Longitud.KILOMETRO:
+                    cantidadAconvertir = cantidadAconvertir / 1000;
+                    break;
+
+                case Longitud.MILLA:
+                    cantidadAconvertir = cantidadAconvertir / 1609;
+                    break;
+
+                case Longitud.PULGADAS:
+                    cantidadAconvertir = cantidadAconvertir * 39.37;
+                    break;
+
+                case Longitud.PIE:
+                    cantidadAconvertir = cantidadAconvertir * 3.281;
+                    break;
+
 
             }
 
@@ -150,6 +227,11 @@ namespace SOF005_Tarea4.Controllers
     public enum Masa
     {
         GRAMO,KILOGRAMO,ONZA,LIBRA
+    }
+
+    public enum Longitud
+    {
+        CENTIMETRO,PIE,PULGADAS,METRO,KILOMETRO,MILLA    
     }
 
 }
