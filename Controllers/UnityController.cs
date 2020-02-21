ac084c426cb7c15f5f5e32d2de60e6e32ead5af6
Nizar <nizar4790k@gmail.com>
2020-02-21 12:53:07 -0400

    se definio la funcion de temperatura

--- Controllers/UnityController.cs
+++ Controllers/UnityController.cs
@@ -9,9 +9,61 @@ namespace SOF005_Tarea4.Controllers
     public class UnityController : Controller
     {
         // GET: Unity
-        public ActionResult Index()
+        public ActionResult Home()
         {
             return View();
         }
+
+
+        private double ConvertirTemperatura(double cantidadAConvertir,Temperatura unidadOrigen,Temperatura unidadDestino)
+        {
+
+            if (unidadOrigen == unidadDestino)
+            {
+                return cantidadAConvertir;
+            }
+
+
+            switch (unidadOrigen)
+            {
+                case Temperatura.KELVIN:
+                     cantidadAConvertir -= 273.15;
+                break;
+
+                case Temperatura.FARENHEIT:
+                    cantidadAConvertir = (cantidadAConvertir - 32) * (5 / 9);
+                break;
+
+
+
+            }
+
+
+            switch (unidadDestino)
+            {
+
+                case Temperatura.FARENHEIT:
+
+                 return   cantidadAConvertir = (cantidadAConvertir * (9 / 5)) + 32;
+
+                case Temperatura.KELVIN:
+
+                    return cantidadAConvertir += 273.15;
+
+                default:
+                    return cantidadAConvertir;
+
+            }
+
+
+    
+        }
+
     }
+
+    public enum Temperatura
+    {
+        CELCIUS,KELVIN,FARENHEIT
+    }
+
 }
