e61c9f50ba9b7158e25602fdd307a27a627bf001
Nizar <nizar4790k@gmail.com>
2020-02-20 21:57:19 -0400

    second commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SOF005_Tarea4.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SOF005_Tarea4.Startup))]
+namespace SOF005_Tarea4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
