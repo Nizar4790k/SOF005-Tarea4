296529271b98fb6afa7b99951c8c49b75759314b
Nizar <nizar4790k@gmail.com>
2020-02-23 07:11:21 -0400

    el modelo del formulario fue anadido

---
+++ Models/Formulario/Empleado.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOF005_Tarea4.Models.Formulario
+{
+    public class Empleado
+    {
+        private string Nombre { get; set; }
+
+        private string Apellido { get; set; }
+
+        private string Cedula { get; set; }
+
+        private int Edad { get; set; }
+
+        private string Telefono { get; set; }
+
+        private string Email { get; set; }
+
+        private Genero Sexo { get; set; }
+
+        private  Puesto Posicion { get; set; }
+
+        private string Foto { get; set; }
+
+        private string Curriculum { get; set; }
+
+
+
+        public enum Genero {
+            MASCULINO,FEMENINO
+            }
+
+        public enum Puesto
+        {
+            SECRETARIO,INGENIERO,ABOGADO,MEDICO
+        }
+
+
+
+    }
+}
