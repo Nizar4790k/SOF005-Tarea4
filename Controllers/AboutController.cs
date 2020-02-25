aad91a2dcd330d9c5e9667df95090c2f37130519
Nizar <nizar4790k@gmail.com>
2020-02-24 22:51:03 -0400

    acerca de: finalizado

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOF005_Tarea4.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Home()
+        {
+
+            
+            return View();
+        }
+    }
+}
