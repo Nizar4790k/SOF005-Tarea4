f560e9e0aae96aec64f236e5f0ef1ae0bcd2e809
RodelinDipre <53545087+RodelinDipre@users.noreply.github.com>
2020-02-26 22:35:09 -0400

    Vista para leer archivo excel y mostrar

--- Controllers/DataController.cs
+++ Controllers/DataController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ExcelDataReader;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +12,56 @@ namespace SOF005_Tarea4.Controllers
     public class DataController : Controller
     {
         // GET: Data
-        public ActionResult Index()
+        public ActionResult excelFile()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult excelFile(HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (upload != null && upload.ContentLength > 0)
+                {
+                    // ExcelDataReader works with the binary Excel file, so it needs a FileStream
+                    // to get started. This is how we avoid dependencies on ACE or Interop:
+                    Stream stream = upload.InputStream;
+
+                    // We return the interface, so that
+                    IExcelDataReader reader = null;
+
+
+                    if (upload.FileName.EndsWith(".xls"))
+                    {
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                    }
+                    else if (upload.FileName.EndsWith(".xlsx"))
+                    {
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "Formato de archivo no soportado");
+                        return View();
+                    }
+
+                    DataSet result = reader.AsDataSet();
+                    reader.Close();
+
+                    return View(result.Tables[0]);
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Porfavor seleccione un archivo");
+                }
+            }
+            return View();
+        }
+
+
 
         public ActionResult Contenido()
         {
