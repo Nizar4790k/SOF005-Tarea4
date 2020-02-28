f84fa0ab62518837a7552da4e8e08bda5e5cfd61
Nizar <nizar4790k@gmail.com>
2020-02-28 13:26:45 -0400

    Se anadio el login sin la authenticacion de facebook

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using SOF005_Tarea4.Models.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOF005_Tarea4.Controllers
+{
+
+    public class LoginController : Controller
+    {
+        // GET: Login
+
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Home","Home2",user);
+            }
+            else
+            {
+                return View();
+            }
+
+
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+
+    }
+}
